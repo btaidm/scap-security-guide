6f5096f0ea547481474249477ab27a8ad97abf9c
Keith Rutkowski <krrutkow@mtu.edu>
2018-04-13 14:18:03 -0400

    Fix checks for PATH variable including current directory

--- archlinux/sce/check-all-user-session.sh
+++ archlinux/sce/check-all-user-session.sh
@@ -2,16 +2,19 @@
 # platform = archlinux
 
 if (
-sudo grep --invert-match '/\(nologin\|false\|git-shell\)$' /etc/passwd | awk -F':' '{print $1 ":" $6}' | while read line ; do
-	username=`echo "$line " | awk -F':' '{print $1}'`
-	homedir=`echo "$line" | awk -F':' '{print $2}'`
-	
-	[ -z "$homedir" ] && exit 1  # each user has a homedir specified
-	sudo find "$homedir" -not -user "$username" || exit 1  # homedir is owned by user
-	sudo find "$homedir" -not -group "$username" || exit 1  # homedir is owned by user's primary group
-	sudo find "$homedir" \( -perm -0007 -o -perm -0020 \) || exit 1  # homedir is not writeable by group and not readable by others
-	sudo find "$homedir" -type f -name '.*' -maxdepth 1 \( -perm -0002 -o -perm -0020 \) || exit 1  # all .bashrc-like files are not writeable by others
-done
+	while read line ; do
+		username=`echo "$line " | awk -F':' '{print $1}'`
+		homedir=`echo "$line" | awk -F':' '{print $2}'`
+		
+		[ -z "$homedir" ] && exit 1  # each user has a homedir specified
+		find "$homedir" -not -user "$username" || exit 1  # homedir is owned by user
+		find "$homedir" -not -group "$username" || exit 1  # homedir is owned by user's primary group
+		find "$homedir" \( -perm -0007 -o -perm -0020 \) || exit 1  # homedir is not writeable by group and not readable by others
+		find "$homedir" -type f -name '.*' -maxdepth 1 \( -perm -0002 -o -perm -0020 \) || exit 1  # all .bashrc-like files are not writeable by others
+		while read dotfile ; do
+			! grep --with-filename '\(^\|\s+\)PATH.*\.' "$dotfile" || exit 1 # check that PATH doesnt get "current" directory added
+		done < <(find "$homedir" -type f -name '.*' -maxdepth 1)
+	done < <(grep --invert-match '/\(nologin\|false\|git-shell\)$' /etc/passwd | awk -F':' '{print $1 ":" $6}')
 ) ; then
 	exit $XCCDF_RESULT_PASS
 else
