1e338de6f10651378d54ea3b74e084a6199e5a95
Keith Rutkowski <krrutkow@mtu.edu>
2018-04-13 16:33:25 -0400

    Add check for account expiration

--- archlinux/sce/check-password-lifetimes.sh
+++ archlinux/sce/check-password-lifetimes.sh
@@ -4,10 +4,15 @@
 if (
 	while read username ; do
 		echo "$username"
+		
 		mindays=`chage --list "$username" | grep '^Minimum number' | awk -F':' '{print $2}' | tr -d '[[:space:]]'`
-		maxdays=`chage --list "$username" | grep '^Maximum number' | awk -F':' '{print $2}' | tr -d '[[:space:]]'`
 		[ "$mindays" -ge "1" ] || exit 1
+		
+		maxdays=`chage --list "$username" | grep '^Maximum number' | awk -F':' '{print $2}' | tr -d '[[:space:]]'`
 		[ "$maxdays" -le "60" ] || exit 1
+		
+		inactive=`chage --list "$username" | grep '^Password inactive' | awk -F':' '{print $2}' | tr -d '[[:space:]]'`
+		[ "$inactive" -le "35" ] || exit 1
 	done < <(grep --invert-match '/\(nologin\|false\|git-shell\)$' /etc/passwd | awk -F':' '{print $1}' | grep --invert-match '^root$')
 ) ; then
 	exit $XCCDF_RESULT_PASS
