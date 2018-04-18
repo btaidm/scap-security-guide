96fb6adab1ab20f53b71fc76705e09389ffd0323
Keith Rutkowski <krrutkow@mtu.edu>
2018-04-18 16:31:31 -0400

    Fix password lifetime check

--- archlinux/sce/check-password-lifetimes.sh
+++ archlinux/sce/check-password-lifetimes.sh
@@ -3,9 +3,9 @@
 
 function add_to_date()
 {
-    olddate=${1:?Need to supply date}
-    days=${2:?Need to supply number of days}
-    format=${3:-"+%s"}
+    olddate=`date` #${1:?Need to supply date}
+    days=${1:?Need to supply number of days}
+    format='+%s'
 
     d1=$(date -d "$olddate" "+%s")
     d2=$(( d1 + (60*60*24) * days ))
@@ -23,12 +23,11 @@ if (
 		maxdays=`chage --list "$username" | grep '^Maximum number' | awk -F':' '{print $2}' | tr -d '[[:space:]]'`
 		[ "$maxdays" -le "60" ] || exit 1
 		
-		inactive=`chage --list "$username" | grep '^Password inactive' | awk -F':' '{print $2}' | tr -d '[[:space:]]'`
-		[ "$inactive" == "never" ] && exit 1
+		inactive=`chage --list "$username" | grep '^Password inactive' | awk -F':' '{print $2}'`
+		[ "$inactive" == " never" ] && exit 1
 		
-		expiredate=$(date -d "$inactive" "+%s")
-		laterdate=$(add_to_date `date` 90)
-		[ "$expiredate" -ge "$laterdate" ] && exit 1
+		echo `date -d "$inactive" '+%s'` -le `add_to_date 90`
+		[ `date -d "$inactive" '+%s'` -le `add_to_date 90` ] || exit 1
 	done < <(grep --invert-match '/\(nologin\|false\|git-shell\)$' /etc/passwd | awk -F':' '{print $1}' | grep --invert-match '^root$')
 ) ; then
 	exit $XCCDF_RESULT_PASS
